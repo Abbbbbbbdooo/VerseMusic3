803d9642707c7d5860364c69f3756a56cc481b17
Abdo <45042294+Abbbbbbbdooo@users.noreply.github.com>
2018-11-22 15:33:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -316,16 +316,17 @@ client.on('message', message => {
     if (message.content === 'help') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
-        .setDescription('**برفكس البوت (!)**')
-        .addField('play', 'لتشغيل اغنية')
-        .addField('join', 'دخول رومك الصوتي')
-        .addField('disconnect', 'الخروج من رومك الصوتي')
-        .addField('skip', 'تخطي الأغنية')
-        .addField('pause', 'ايقاف الاغنية مؤقتا')
-        .addField('resume', 'تكملة الاغنية')
-        .addField('queue', 'اظهار قائمة التشغيل')
-        .addField('np', 'اظهار الاغنية اللي انت مشغلها حاليا')
-        .setFooter('(general_commands) لاظهار الاوامر العامة')
+        .setDescription('**برفكس البوت (3)**')
+        .addField('Play', 'لتشغيل اغنية')
+        .addField('Join', 'دخول رومك الصوتي')
+        .addField('Disconnect', 'الخروج من رومك الصوتي')
+        .addField('Skip', 'تخطي الأغنية')
+        .addField('Pause', 'ايقاف الاغنية مؤقتا')
+        .addField('Resume', 'تكملة الاغنية')
+        .addField('Queue', 'اظهار قائمة التشغيل')
+        .addField('Np', 'اظهار الاغنية اللي انت مشغلها حاليا')
+        .addField('Repart','لأعادة الاغنية')
+        .setFooter('(General_commands) لاظهار الاوامر العامة')
       message.channel.send(helpEmbed);
     }
 });
@@ -349,7 +350,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`3play | Forest Music`,"http://twitch.tv/Forest Server")
+client.user.setGame(`3play | Verse Music`,"http://twitch.tv/Forest Server")
 client.user.setStatus("dnd")
 });
 
